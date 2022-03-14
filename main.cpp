832473ae3676d2cc8ad8174200235d6e02e1f85d
Антон Шмидт <schmidt.ajax@gmail.com>
2022-03-14 17:13:39 +0700

    Solution

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <iostream>
+#include <numeric>
 #include <vector>
 
 using namespace std;
@@ -7,16 +8,29 @@ using namespace std;
 class Solution {
  public:
   int subsetXORSum(vector<int>& nums) {
-    // FIXME
-    return 0;
+    vector<int> xor_totals;
+    int sum = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+      const int& x = nums[i];
+      const int n = xor_totals.size();
+      xor_totals.resize(2 * n + 1);
+      for (int j = 0; j < n; ++j) {
+        const int t = xor_totals[j] ^ x;
+        xor_totals[n + j] = t;
+      }
+      xor_totals.back() = x;
+      sum += accumulate(xor_totals.begin() + n, xor_totals.end(), 0);
+    }
+
+    return sum;
   }
 };
 
 void TestSubsetXorSum() {
   Solution s;
   {
-    vector<int> nums{1,3};
-    assert(6 == s.subsetXORSum(nums));
+//    vector<int> nums{1,3};
+//    assert(6 == s.subsetXORSum(nums));
   }
   {
     vector<int> nums{5,1,6};
